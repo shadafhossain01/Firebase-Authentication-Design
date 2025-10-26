058314db474f1d6049ccfbc26f9c61d57de561ff
Shadaf Hossain <shadafhossain01@gmail.com>
2025-10-26 20:40:04 +0600

    Routes Setup Done

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './page/Login.jsx';
+import SignUp from './page/SignUp.jsx';
 
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/signup" element={<SignUp/>} />
+    </Routes>
+  </BrowserRouter>
+);
