c581c3d269f037c86f5fe31668224fe13badc9b3
Shadaf Hossain <shadafhossain01@gmail.com>
2025-10-27 00:08:48 +0600

    Signup Form Design Done

---
+++ src/components/signup/SignUpForm.jsx
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom";
+import SocialIcon from "../common/SocialIcon";
+
+const SignUpForm = () => {
+  return (
+    <div className="flex justify-center items-center min-h-[90vh] flex-col">
+      <div className="bg-white shadow-xl p-10 rounded-[10px]">
+        <h2 className="font-bold text-center text-2xl mb-8">Sign up Form</h2>
+
+        {/* Form Start Here */}
+        <form>
+          {/* Email Field */}
+          <div className="space-y-3">
+            <label className="block font-medium">Email:</label>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="mb-[15px] w-[350px] border border-gray-300 focus:outline-none py-[5px] px-[15px] rounded-[5px] focus:border-blue-500"
+            />
+          </div>
+          {/* Password Field */}
+          <div className=" space-y-3">
+            <label className="block font-medium">Password:</label>
+            <input
+              type="text"
+              placeholder="Enter your password"
+              className="mb-[25px] w-[350px] border border-gray-300 focus:outline-none py-[5px] px-[15px] rounded-[5px] focus:border-blue-500"
+            />
+          </div>
+          {/* Login Btn */}
+          <button className="btn bg-blue-600 block w-full p-[7px] rounded-[5px] cursor-pointer text-white">
+            Sign up
+          </button>
+        </form>
+
+        <p className="font-light text-center my-5 text-[17px]">Or Sign up with</p>
+
+        {/* Social Icons */}
+        <SocialIcon />
+
+        <p className=" text-center mt-[27px] text-[16px]">
+          Do have any account? Plaese{" "}
+          <Link to="/login" className="text-blue-500 font-medium">
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpForm;
