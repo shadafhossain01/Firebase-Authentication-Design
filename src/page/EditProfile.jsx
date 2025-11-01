693ada7dfd9541a81ad0e60e5344845e62c37c71
Shadaf Hossain <shadafhossain01@gmail.com>
2025-11-01 10:42:35 +0600

    Add Left Arrow in Edit Profile Page

--- src/page/EditProfile.jsx
+++ src/page/EditProfile.jsx
@@ -1,15 +1,25 @@
+import { FiArrowLeftCircle } from "react-icons/fi";
 import ProfileCard from "../components/profile/ProfileCard";
 import ProfileForm from "../components/profile/ProfileForm";
+import { useNavigate } from "react-router-dom";
 
 const EditProfile = () => {
-  return <div className="flex gap-x-[140px] items-center h-[90vh] w-[900px] mx-auto">
-  <div className="w-[45%]">
-    <ProfileCard/>
-  </div>
-  <div className="w-[40%]">
-    <ProfileForm/>
-  </div>
-  </div>;
+  const navigate=useNavigate()
+  const handleBack=()=>{
+    navigate(-1)
+  }
+
+  return (
+    <div className="flex gap-x-[140px] items-center h-[85vh] w-[900px] mx-auto">
+      <div className="w-[45%]">
+        <FiArrowLeftCircle className="text-[28px] mb-[31px] cursor-pointer" onClick={handleBack}/>
+        <ProfileCard />
+      </div>
+      <div className="w-[40%] mt-[31px]">
+        <ProfileForm />
+      </div>
+    </div>
+  );
 };
 
 export default EditProfile;
